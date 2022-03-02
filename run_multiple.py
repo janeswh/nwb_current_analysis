b3666654d49948c9bb60007694c53b8d21918766
Jane Huang <62308312+janeswh@users.noreply.github.com>
2022-03-02 17:32:51 -0500

    trying to import run_single into run_multiple

--- run_multiple.py
+++ run_multiple.py
@@ -1,4 +1,6 @@
 import os
+import run_single_test
+from single_test import JaneCell
 
 dataset = 'non-injected'
 data_folder = os.path.join('/home/jhuang/Documents/phd_projects/MMZ_STC_dataset/data', dataset)
@@ -7,3 +9,14 @@ nwbfile_list = []
 for file in os.listdir(data_folder):
     if file.endswith(".nwb"):
         nwbfile_list.append(file)
+
+
+# need to assign the same project dataset variable to all cells 
+# dataset variable determines path location to tables and figures
+
+
+# phd_projects/MMZ_STC_dataset/tables
+#   non-injected
+#   5dpi
+#   3 dpi, etc
+
