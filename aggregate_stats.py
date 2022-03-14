0b65616ec3c72d7c9bc16834ce37273f05a897dc
Jane Huang <62308312+janeswh@users.noreply.github.com>
2022-03-14 15:51:07 -0400

    added looping through genotypes in run_multiple

--- aggregate_stats.py
+++ aggregate_stats.py
@@ -11,234 +11,258 @@ pio.renderers.default = "browser"
 import pdb
 import glob
 
-# def run_aggregate_stats(dataset):
 
-# def get_summary_stats():
+class GenotypeSummary(object):
+    def __init__(self, dataset, genotype, tables_folder, figures_folder):
+        self.dataset = dataset
+        self.genotype = genotype
+        self.genotype_stats_folder = os.path.join(
+            tables_folder, dataset, genotype
+        )
+        self.genotype_figures_folder = os.path.join(
+            figures_folder, dataset, genotype
+        )
+        self.concat_df = self.get_summary_stats()
 
-dataset = "5dpi"
-dataset_stats_folder = os.path.join(
-    "/home/jhuang/Documents/phd_projects/MMZ_STC_dataset/tables", dataset
-)
+    def get_summary_stats(self):
+        # reads in all the summary stats values
+        file_paths = []
+        for filename in os.listdir(os.path.join(self.genotype_stats_folder)):
+            if filename.endswith("response_stats.csv"):
+                file_paths.append(
+                    os.path.join(self.genotype_stats_folder, filename)
+                )
+        concat_df = pd.concat(
+            [
+                pd.read_csv(file, index_col=None, header=0)
+                for file in file_paths
+            ]
+        )
 
-# listing the genotypes in each dataset
-genotypes_list = [
-    genotype.name
-    for genotype in os.scandir(dataset_stats_folder)
-    if genotype.is_dir()
-]
+        return concat_df
 
-# testing on one genotype
-genotype = genotypes_list[0]
+    def calc_summary_avgs(self):
+        # this calculates stats grouped by stim conditions
+        self.mean_df = self.concat_df.groupby(
+            ["Light Intensity", "Light Duration"]
+        ).mean()
+        self.sem_df = self.concat_df.groupby(
+            ["Light Intensity", "Light Duration"]
+        ).sem()
 
-stats_folder = os.path.join(dataset_stats_folder, genotype)
+        # this creates a list of unique stim condition combinations and allows iteration
+        stim_conditions = (
+            self.concat_df[["Light Intensity", "Light Duration"]]
+            .drop_duplicates(ignore_index=True)
+            .copy()
+        )
 
+        zip_conditions = zip(
+            stim_conditions["Light Intensity"],
+            stim_conditions["Light Duration"],
+        )
 
-for genotype in genotypes_list:
-    stats_folder = os.path.join(dataset_stats_folder, genotype)
-# testing loop on one genotype
+        # this allows display of individual cell values matching each stim condition
+        for intensity, duration in zip_conditions:
+            indiv_cells_df = self.concat_df[
+                (self.concat_df["Light Intensity"] == intensity)
+                & (self.concat_df["Light Duration"] == duration)
+            ].copy()
 
-genotype = "Gg8"
+    def plot_averages(self):
 
+        # plotting
+        durations = self.concat_df["Light Duration"].unique()
+        intensities = self.concat_df["Light Intensity"].unique()
+        color_dict = {
+            " 2 ms": "#7D1935",
+            " 1 ms": "#B42B51",
+            " 0.25 ms": "#E63E6D",
+            " 0.01 ms": "#F892B9",
+        }
+        summary_stats_fig = make_subplots(
+            rows=3, cols=2, x_title="Light Intensity (%)"
+        )
 
-file_paths = []
-for filename in os.listdir(os.path.join(stats_folder)):
-    if filename.endswith("response_stats.csv"):
-        file_paths.append(os.path.join(stats_folder, filename))
-concat_df = pd.concat(
-    [pd.read_csv(file, index_col=None, header=0) for file in file_paths]
-)
+        x_intensity = self.concat_df["Light Intensity"].unique()
 
-# this calculates stats grouped by stim conditions
-mean_df = concat_df.groupby(["Light Intensity", "Light Duration"]).mean()
-sem_df = concat_df.groupby(["Light Intensity", "Light Duration"]).sem()
+        for count, duration in enumerate(durations):
+            # mean trace peak amplitude
+            summary_stats_fig.add_trace(
+                go.Box(
+                    x=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Light Intensity"],
+                    ].squeeze(),
+                    y=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Mean Trace Peak (pA)"],
+                    ].squeeze(),
+                    name=duration,
+                    line=dict(color=color_dict[duration]),
+                    legendgroup=duration,
+                ),
+                row=1,
+                col=1,
+            )
 
-# this creates a list of unique stim condition combinations and allows iteration
-stim_conditions = (
-    concat_df[["Light Intensity", "Light Duration"]]
-    .drop_duplicates(ignore_index=True)
-    .copy()
-)
+            # Mean Onset Latency (ms)
+            summary_stats_fig.add_trace(
+                go.Box(
+                    x=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Light Intensity"],
+                    ].squeeze(),
+                    y=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Mean Onset Latency (ms)"],
+                    ].squeeze(),
+                    name=duration,
+                    line=dict(color=color_dict[duration]),
+                    legendgroup=duration,
+                ),
+                row=1,
+                col=2,
+            )
 
-zip_conditions = zip(
-    stim_conditions["Light Intensity"], stim_conditions["Light Duration"]
-)
+            # onset jitter
+            summary_stats_fig.add_trace(
+                go.Box(
+                    x=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Light Intensity"],
+                    ].squeeze(),
+                    y=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Onset Jitter"],
+                    ].squeeze(),
+                    name=duration,
+                    line=dict(color=color_dict[duration]),
+                    legendgroup=duration,
+                ),
+                row=2,
+                col=1,
+            )
 
-# this allows display of individual cell values matching each stim condition
-for intensity, duration in zip_conditions:
-    indiv_cells_df = concat_df[
-        (concat_df["Light Intensity"] == intensity)
-        & (concat_df["Light Duration"] == duration)
-    ].copy()
+            # mean trace onset latency
+            summary_stats_fig.add_trace(
+                go.Box(
+                    x=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Light Intensity"],
+                    ].squeeze(),
+                    y=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Mean Trace Onset Latency (ms)"],
+                    ].squeeze(),
+                    name=duration,
+                    line=dict(color=color_dict[duration]),
+                    legendgroup=duration,
+                ),
+                row=2,
+                col=2,
+            )
 
+            # mean time to peak
+            summary_stats_fig.add_trace(
+                go.Box(
+                    x=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Light Intensity"],
+                    ].squeeze(),
+                    y=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Mean Time to Peak (ms)"],
+                    ].squeeze(),
+                    name=duration,
+                    line=dict(color=color_dict[duration]),
+                    legendgroup=duration,
+                ),
+                row=3,
+                col=1,
+            )
 
-# plotting
-durations = concat_df["Light Duration"].unique()
-intensities = concat_df["Light Intensity"].unique()
-color_dict = {
-    " 2 ms": "#7D1935",
-    " 1 ms": "#B42B51",
-    " 0.25 ms": "#E63E6D",
-    " 0.01 ms": "#F892B9",
-}
-curve_stats_fig = make_subplots(rows=3, cols=2, x_title="Light Intensity (%)")
+            # mean trace time to peak
+            summary_stats_fig.add_trace(
+                go.Box(
+                    x=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Light Intensity"],
+                    ].squeeze(),
+                    y=self.concat_df.loc[
+                        self.concat_df["Light Duration"] == duration,
+                        ["Mean Trace Time to Peak (ms)"],
+                    ].squeeze(),
+                    name=duration,
+                    line=dict(color=color_dict[duration]),
+                    legendgroup=duration,
+                ),
+                row=3,
+                col=2,
+            )
 
-x_intensity = concat_df["Light Intensity"].unique()
+            # Update xaxis properties
+            # summary_stats_fig.update_xaxes(autorange="reversed")
+            # this defines the intensities order for x-axes
+            summary_stats_fig.update_xaxes(
+                categoryorder="array", categoryarray=np.flip(intensities)
+            )
 
-for count, duration in enumerate(durations):
-    # mean trace peak amplitude
-    curve_stats_fig.add_trace(
-        go.Box(
-            x=concat_df.loc[
-                concat_df["Light Duration"] == duration, ["Light Intensity"],
-            ].squeeze(),
-            y=concat_df.loc[
-                concat_df["Light Duration"] == duration,
-                ["Mean Trace Peak (pA)"],
-            ].squeeze(),
-            name=duration,
-            line=dict(color=color_dict[duration]),
-            legendgroup=duration,
-        ),
-        row=1,
-        col=1,
-    )
+            # Update yaxis properties
+            summary_stats_fig.update_yaxes(
+                title_text="Mean Response Amplitude (pA)",
+                row=1,
+                col=1,
+                autorange="reversed",
+            )
+            summary_stats_fig.update_yaxes(
+                title_text="Mean Onset Latency (ms)", row=1, col=2
+            )
+            summary_stats_fig.update_yaxes(
+                title_text="Mean Onset Jitter", row=2, col=1
+            )
+            summary_stats_fig.update_yaxes(
+                title_text="Mean Trace Onset Latency (ms)", row=2, col=2
+            )
+            summary_stats_fig.update_yaxes(
+                title_text="Mean Time to Peak (ms)", row=3, col=1
+            )
+            summary_stats_fig.update_yaxes(
+                title_text="Mean Trace Time to Peak (ms)", row=3, col=2
+            )
 
-    # Mean Onset Latency (ms)
-    curve_stats_fig.add_trace(
-        go.Box(
-            x=concat_df.loc[
-                concat_df["Light Duration"] == duration, ["Light Intensity"],
-            ].squeeze(),
-            y=concat_df.loc[
-                concat_df["Light Duration"] == duration,
-                ["Mean Onset Latency (ms)"],
-            ].squeeze(),
-            name=duration,
-            line=dict(color=color_dict[duration]),
-            legendgroup=duration,
-        ),
-        row=1,
-        col=2,
-    )
+        summary_stats_fig.update_layout(
+            # yaxis_title='Onset Latency (ms)',
+            boxmode="group"  # group together boxes of the different traces for each value of x
+        )
 
-    # onset jitter
-    curve_stats_fig.add_trace(
-        go.Box(
-            x=concat_df.loc[
-                concat_df["Light Duration"] == duration, ["Light Intensity"],
-            ].squeeze(),
-            y=concat_df.loc[
-                concat_df["Light Duration"] == duration, ["Onset Jitter"],
-            ].squeeze(),
-            name=duration,
-            line=dict(color=color_dict[duration]),
-            legendgroup=duration,
-        ),
-        row=2,
-        col=1,
-    )
+        # below is code from stack overflow to hide duplicate legends
+        names = set()
+        summary_stats_fig.for_each_trace(
+            lambda trace: trace.update(showlegend=False)
+            if (trace.name in names)
+            else names.add(trace.name)
+        )
 
-    # mean trace onset latency
-    curve_stats_fig.add_trace(
-        go.Box(
-            x=concat_df.loc[
-                concat_df["Light Duration"] == duration, ["Light Intensity"],
-            ].squeeze(),
-            y=concat_df.loc[
-                concat_df["Light Duration"] == duration,
-                ["Mean Trace Onset Latency (ms)"],
-            ].squeeze(),
-            name=duration,
-            line=dict(color=color_dict[duration]),
-            legendgroup=duration,
-        ),
-        row=2,
-        col=2,
-    )
+        summary_stats_fig.update_layout(
+            legend_title_text="Light Duration",
+            title_text=(
+                self.dataset + " " + self.genotype + " summary values"
+            ),
+            title_x=0.5,
+        )
 
-    # mean time to peak
-    curve_stats_fig.add_trace(
-        go.Box(
-            x=concat_df.loc[
-                concat_df["Light Duration"] == duration, ["Light Intensity"],
-            ].squeeze(),
-            y=concat_df.loc[
-                concat_df["Light Duration"] == duration,
-                ["Mean Time to Peak (ms)"],
-            ].squeeze(),
-            name=duration,
-            line=dict(color=color_dict[duration]),
-            legendgroup=duration,
-        ),
-        row=3,
-        col=1,
-    )
+        summary_stats_fig.show()
+        self.summary_stats_fig = summary_stats_fig
 
-    # mean trace time to peak
-    curve_stats_fig.add_trace(
-        go.Box(
-            x=concat_df.loc[
-                concat_df["Light Duration"] == duration, ["Light Intensity"],
-            ].squeeze(),
-            y=concat_df.loc[
-                concat_df["Light Duration"] == duration,
-                ["Mean Trace Time to Peak (ms)"],
-            ].squeeze(),
-            name=duration,
-            line=dict(color=color_dict[duration]),
-            legendgroup=duration,
-        ),
-        row=3,
-        col=2,
-    )
+    def save_summary_stats_fig(self):
+        """
+        Saves the summary stats plot as one html file
+        """
 
-    # Update xaxis properties
-    # curve_stats_fig.update_xaxes(autorange="reversed")
-    # this defines the intensities order for x-axes
-    curve_stats_fig.update_xaxes(
-        categoryorder="array", categoryarray=np.flip(intensities)
-    )
+        html_filename = "{}_summary_avgs.html".format(self.genotype)
+        path = os.path.join(self.genotype_figures_folder, html_filename)
 
-    # Update yaxis properties
-    curve_stats_fig.update_yaxes(
-        title_text="Mean Response Amplitude (pA)",
-        row=1,
-        col=1,
-        autorange="reversed",
-    )
-    curve_stats_fig.update_yaxes(
-        title_text="Mean Onset Latency (ms)", row=1, col=2
-    )
-    curve_stats_fig.update_yaxes(title_text="Mean Onset Jitter", row=2, col=1)
-    curve_stats_fig.update_yaxes(
-        title_text="Mean Trace Onset Latency (ms)", row=2, col=2
-    )
-    curve_stats_fig.update_yaxes(
-        title_text="Mean Time to Peak (ms)", row=3, col=1
-    )
-    curve_stats_fig.update_yaxes(
-        title_text="Mean Trace Time to Peak (ms)", row=3, col=2
-    )
-
-curve_stats_fig.update_layout(
-    # yaxis_title='Onset Latency (ms)',
-    boxmode="group"  # group together boxes of the different traces for each value of x
-)
-
-# below is code from stack overflow to hide duplicate legends
-names = set()
-curve_stats_fig.for_each_trace(
-    lambda trace: trace.update(showlegend=False)
-    if (trace.name in names)
-    else names.add(trace.name)
-)
-
-curve_stats_fig.update_layout(
-    legend_title_text="Light Duration",
-    title_text=(dataset + " " + genotype + " summary values"),
-    title_x=0.5,
-)
-
-
-curve_stats_fig.show()
-
+        self.summary_stats_fig.write_html(
+            path, full_html=False, include_plotlyjs="cdn"
+        )
--- run_multiple.py
+++ run_multiple.py
@@ -1,6 +1,8 @@
 import os
 from run_single_test import run_single
-from single_test import JaneCell
+
+# from single_test import JaneCell
+from aggregate_stats import GenotypeSummary
 import pdb
 
 
@@ -34,17 +36,46 @@ def run_dataset_analysis(data_folder, dataset, tables_folder):
         )
 
 
+def get_genotypes(tables_folder, dataset):
+    # listing the genotypes in each dataset
+    stats_folder = os.path.join(tables_folder, dataset)
+    genotypes_list = [
+        genotype.name
+        for genotype in os.scandir(stats_folder)
+        if genotype.is_dir()
+    ]
+
+    if len(genotypes_list) == 0:
+        print(
+            "{} dataset has no cells with responses, no summary stats calculated.".format(
+                dataset
+            )
+        )
+    return genotypes_list
+
+
 def main():
     # gets the list of datasets from file directory
     dataset_list = get_datasets(data_folder, ignored)
-    # dataset_list = ["dox_3dpi"]
+    # dataset_list = ["dox_3dpi_ctrl"]
 
     # runs stats analysis for each dataset
     for dataset_count, dataset in enumerate(dataset_list):
         print("***Starting analysis for {} dataset.***".format(dataset))
-        run_dataset_analysis(
-            data_folder, dataset, tables_folder,
-        )
+        # run_dataset_analysis(
+        #     data_folder, dataset, tables_folder,
+        # )
+        # pdb.set_trace()
+        genotypes_list = get_genotypes(tables_folder, dataset)
+        for genotype in genotypes_list:
+            genotype_summary = GenotypeSummary(
+                dataset, genotype, tables_folder, figures_folder
+            )
+            genotype_summary.get_summary_stats()
+            genotype_summary.calc_summary_avgs()
+            genotype_summary.plot_averages()
+            genotype_summary.save_summary_stats_fig()
+
         print(
             "***Analysis for {} dataset done, #{}/{} datasets.***".format(
                 dataset, dataset_count + 1, len(dataset_list)
@@ -57,6 +88,9 @@ if __name__ == "__main__":
     ignored = {"esc_unusable"}
     tables_folder = (
         "/home/jhuang/Documents/phd_projects/MMZ_STC_dataset/tables"
+    )
+    figures_folder = (
+        "/home/jhuang/Documents/phd_projects/MMZ_STC_dataset/figures"
     )
 
     main()
