62b45a557362f3f2fcb836c2c437c0780ae15748
Jane Huang <62308312+janeswh@users.noreply.github.com>
2022-03-16 11:43:55 -0400

    moved file setting to global variables file

---
+++ file_settings.py
@@ -0,0 +1,8 @@
+"""File settings for running multiple analyses across datasets"""
+
+data_folder = "/home/jhuang/Documents/phd_projects/MMZ_STC_dataset/data"
+ignored = {"esc_unusable"}
+tables_folder = "/home/jhuang/Documents/phd_projects/MMZ_STC_dataset/tables"
+figures_folder = "/home/jhuang/Documents/phd_projects/MMZ_STC_dataset/figures"
+
+threshold_list = [2, 4]
