c9886c59deb808c6c9d475d5ae0be127a14a3a08
Jane Huang <62308312+janeswh@users.noreply.github.com>
2022-03-18 15:52:49 -0400

    added 1 ms onset latency threshold

--- file_settings.py
+++ file_settings.py
@@ -5,6 +5,6 @@ ignored = {"esc_unusable"}
 tables_folder = "/home/jhuang/Documents/phd_projects/MMZ_STC_dataset/tables"
 figures_folder = "/home/jhuang/Documents/phd_projects/MMZ_STC_dataset/figures"
 
-threshold_list = ["nothresh", 2, 4]
+threshold_list = ["nothresh", 1, 2, 4]
 selected_condition = ("100%", " 1 ms")
 
