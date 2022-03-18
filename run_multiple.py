3dc043e1f8df8fc45a5f881564b5c69e621f9eec
Jane Huang <62308312+janeswh@users.noreply.github.com>
2022-03-18 18:05:03 -0400

    remove plotly import

--- run_multiple.py
+++ run_multiple.py
@@ -7,10 +7,6 @@ import file_settings
 
 import pdb
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import plotly.express as px
-
 
 def get_datasets():
     dataset_list = [
